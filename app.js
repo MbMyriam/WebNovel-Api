d2bde97c29136ef3d6a830963650850192058307
MbMyriam <myriam.mb@ynov.com>
2023-11-16 16:37:39 +0100

    maj du app et server

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const http = require('http');
+// Déclaration des modules à importer
+const express = require('express')
+const app = express()
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Utilisation de middleware globaux
+app.use(express.json()); // Permet de parser automatiquement le json en entrée
 
-const server = http.createServer((req, res) => {
+app.use((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello world');
 });
+
+// Déclaration globale du middleware d'erreur, on assume que le paramètre error, possède certains attributs
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+        .json({success: false, message: error.message, status: error.status});
+});
+
+
+
+// On oublie pas d'exporter pour tester
+module.exports = app
--- server.js
+++ server.js
@@ -1,21 +1,5 @@
-const express = require('express');
+const app = require('./app')
 
-const app = express();
-
-const port = 3000;
-
-app.get('/', (req, res, next) => {
-    console.log(req.params);
-    console.log(req.query);
-    console.log(req.body);
-
-
-    res.json({
-        sucess:true,
-        message: "HELLO WORLD"
-    });
-});
-
-app.listen( port, () => {
-    console.log('Server IS running');
-});
+app.listen(3000, () => {
+    console.log('Server running on port 3000 !')
+})
