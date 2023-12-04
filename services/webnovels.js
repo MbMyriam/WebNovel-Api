56d640a1be93670cf6d046612001654f54ad1cb2
MbMyriam <myriam.mb@ynov.com>
2023-12-04 10:16:20 +0100

    Création du service pour webnovels

---
+++ services/webnovels.js
@@ -0,0 +1,9 @@
+let webnovels = [
+    {id: 1, title: "Premier novel", chapiter: [{number: 1, title: "Premier chapitre"}]}, 
+    {id: 2, title: "Second novel", chapiter: [{number: 2, title: "Deuxième chapitre"}]}, 
+    {id: 3, title: "Troisième novel", chapiter: [{number: 3, title: "Troisième chapitre"}]}
+];
+
+exports.getAllNovels = () => {
+    return webnovels;
+}
