e76f503f48e461600e7cdc8bfebd947a5408286a
MbMyriam <myriam.mb@ynov.com>
2023-12-04 09:49:24 +0100

    Maj et configuration openApi

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // Déclaration des modules à importer
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const OpenApiValidator = require('express-openapi-validator');
 
 // Utilisation de middleware globaux
 app.use(express.json()); // Permet de parser automatiquement le json en entrée
+
+app.use(
+    OpenApiValidator.middleware({
+      apiSpec: './open-api.yaml',
+      ignoreUndocumented: true
+    }),
+  );
+
+const spec = path.join(__dirname, './open-api.yaml');
+app.use('/spec', express.static(spec));
 
 app.use((req, res) => {
     res.statusCode = 200;
@@ -16,7 +27,6 @@ app.use((error, req, res, next) => {
     res.status(error.status || 500)
         .json({success: false, message: error.message, status: error.status});
 });
-
 
 
 // On oublie pas d'exporter pour tester
